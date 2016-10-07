3ac935e0203852bd0fbe8b153a152ab5407f7f36
Aad Versteden <madnificent@gmail.com>
2016-10-07 17:08:23 +0200

    Use git link to altered plug_proxy
    We have altered this library so we can have an engine that maps over the
    received content and that can alter it.  The local link will not work
    externally.

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule UsePlugProxy.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:plug_proxy, git: "file:///home/madnificent/code/elixir/plug-proxy/"},
+    [{:plug_proxy, git: "https://github.com/madnificent/plug-proxy.git"},
      {:poison, "~> 2.0"}]
   end
 end
