cc1cff4e0323898e5007e654b24a7f44988a3e3a
Aad Versteden <madnificent@gmail.com>
2019-09-17 14:43:01 +0200

    Increase max header size for CLEAR_KEYS and amount of allowed calls
    The mu-cache serves as middleware between two entities.  We're
    increasing the total number of calls it can maintain and we're
    increasing the header value length to cater for larger clear-key
    updates.

--- lib/mu_elixir_cache.ex
+++ lib/mu_elixir_cache.ex
@@ -1,19 +1,23 @@
 defmodule MuElixirCache do
-
   use Application
 
   def start(_type, _args) do
-    IO.puts "Starting application"
+    IO.puts("Starting application")
     # List all child processes to be supervised
     children = [
-      {UsePlugProxy.Cache,%{}},
-      {Plug.Cowboy, scheme: :http, plug: UsePlugProxy, options: [port: 80]}
-     ]
+      {UsePlugProxy.Cache, %{}},
+      {Plug.Cowboy,
+       scheme: :http,
+       plug: UsePlugProxy,
+       options: [
+         port: 80,
+         protocol_options: [max_header_value_length: 409_600_000, max_keepalive: 1000]
+       ]}
+    ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one]
     Supervisor.start_link(children, opts)
   end
-
 end
