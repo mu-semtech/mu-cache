5a93038df23057d026d5efec0337a77bf73015ff
Erika Pauwels <erika.pauwels@gmail.com>
2021-03-01 17:29:08 +0100

    Release v2.0.2
    Bugfix:
    - prevent double / in proxying to backend

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule UsePlugProxy.Mixfile do
 
   def project do
     [app: :use_plug_proxy,
-     version: "2.0.1",
+     version: "2.0.2",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
