f92b320d60a0c973d7fda7a490a617205be9c880
Aad Versteden <madnificent@gmail.com>
2026-09-24 18:14:57 +0200

    Only send out notifications when paths match
    We sent out an empty sparql query in the past but that carries no value.

--- lib/cache/notify.ex
+++ lib/cache/notify.ex
@@ -24,53 +24,49 @@ defmodule Cache.Notify do
     { :noreply, state }
   end
   def handle_cast(keys, state) do
-    # keys_to_remove
-    # |> Enum.map( fn
-    #   ({_method, _full_path, "", _allowed_groups, rewrite_url}) -> rewrite_url
-    #   ({_method, _full_path, query_params, _allowed_groups, rewrite_url}) -> rewrite_url <> "?" <> query_params
-    # end )
-    # |> Cache.Notify.clear()
+    clear_combinations =
+      keys
+      |> Enum.map( fn
+        {_method, _full_path, "", allowed_groups, rewrite_url} -> { rewrite_url, allowed_groups }
+        {_method, _full_path, query_params, allowed_groups, rewrite_url } -> { rewrite_url <> "?" <> query_params, allowed_groups }
+      end )
+      |> Enum.filter( fn {path, _} -> matches_pattern? path end )
+      |> IO.inspect( label: "Clear combinations" )
 
-    clear_resources = keys
-    |> Enum.map( fn
-      {_method, _full_path, "", allowed_groups, rewrite_url} -> { rewrite_url, allowed_groups }
-      {_method, _full_path, query_params, allowed_groups, rewrite_url } -> { rewrite_url <> "?" <> query_params, allowed_groups }
-    end )
-    |> Enum.map( fn {path, allowed_groups} ->
-      if matches_pattern? path do
-        uuid = UUID.uuid4();
-        uri = "http://services.semantic.works/cache/resources/#{uuid}"
-
-        "
-          <#{uri}>
-            a cache:Clear;
-            mu:uuid #{sparql_escape_string(uuid)};
-            cache:path #{sparql_escape_string(path)};
-            cache:allowedGroups #{sparql_escape_string(allowed_groups)}.
+    unless Enum.empty? clear_combinations do
+      clear_resources =
+        clear_combinations
+        |> Enum.map(
+          fn {path, allowed_groups} ->
+            uuid = UUID.uuid4();
+            uri = "http://services.semantic.works/cache/resources/#{uuid}"
+            # TODO: make path based on redirected URL
             "
-        # TODO: make path based on redirected URL
-      else
-        ""
-      end
-    end)
-    |> Enum.join("\n")
+              <#{uri}>
+              a cache:Clear;
+              mu:uuid #{sparql_escape_string(uuid)};
+              cache:path #{sparql_escape_string(path)};
+              cache:allowedGroups #{sparql_escape_string(allowed_groups)}.
+            "
+          end)
+        |> Enum.join("\n")
 
-    query = "PREFIX cache: <http://mu.semte.ch/vocabularies/cache/>
-      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-      INSERT DATA {
-        #{clear_resources}
-      }"
+      query = "PREFIX cache: <http://mu.semte.ch/vocabularies/cache/>
+        PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+        INSERT DATA {
+          #{clear_resources}
+        }"
 
-    IO.puts(query)
+      headers = [
+        {"mu-auth-scope", "http://services.semantic.works/cache"},
+        {"content-type", "application/sparql-update"},
+        {"accept", "application/sparql-results+json"}
+      ]
+      endpoint = "http://database:8890/sparql"
 
-    headers = [
-      {"mu-auth-scope", "http://services.semantic.works/cache"},
-      {"content-type", "application/sparql-update"},
-      {"accept", "application/sparql-results+json"}
-    ]
-    endpoint = "http://database:8890/sparql"
+      {:ok, _response} = HTTPoison.post(endpoint, query, headers) # TODO: retry on failure
+    end
 
-    {:ok, _response} = HTTPoison.post(endpoint, query, headers) # TODO: retry on failure
     {:noreply, state}
   end
 
